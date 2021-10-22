1ea90cf07e2f0fd467d2994563db41545d693b7b
Bashar Ahmed <61731078+besho58@users.noreply.github.com>
2021-10-22 12:24:39 +0200

    Cleared var warnings

--- src/views/todos.js
+++ src/views/todos.js
@@ -4,7 +4,7 @@ const useForceUpdate = ()=> {
   // making a fake state to force the functional component to update
   // alt for forceUpdate()
   // The hook can only be called to in class or functional
-  const [value, setValue] = useState(0);
+  const setValue = useState(0);
   return ()=> setValue( value => value + 1);
 }
 
